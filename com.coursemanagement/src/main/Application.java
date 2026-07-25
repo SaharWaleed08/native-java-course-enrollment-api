bbe4f59340211e459969e9e9e38c4cd9c28f0119
Sahar <wsahar883@gmail.com>
2026-07-25 03:20:26 +0300

    Initialize native Java project

---
+++ com.coursemanagement/src/main/Application.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Application {
+    public static void main(String[] args){
+        System.out.println("Course Enrollment Management System\n" +
+                "Application started successfully");
+    }
+}
